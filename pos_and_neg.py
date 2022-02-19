e0708119950ea968e48f7bf6055ceef9c6eef97a
Michael Wambua <michaelmutisyawambua@gmail.com>
2022-02-19 21:57:13 +0300

    added an iterative that checks vowels and consonants

--- pos_and_neg.py
+++ pos_and_neg.py
@@ -1,5 +1,8 @@
 # we will be iterating through a list and appending two other lists one for poitive integers and the other for negative integers
 
+from asyncio import constants
+
+
 L=[1,2,5,7,-1,3,-6,7]
 p=[]
 n=[]
@@ -11,3 +14,17 @@ for num in L:
 
 print ('the list of positive numbers is \t :',p)
 print('the list of negative numbers \t : ',n)
+
+string=input('enter a string \t : ')
+vowels=""
+constant=""
+for i in string:
+    if ((i=='a') |(i=='e') |(i=='i')|(i=='o')|(i=='u')):
+        vowels=vowels+str(i)
+
+    else:
+        constant=constant+str(i)
+
+print('the vowels in your string is/are \t :',vowels)
+print('the consonant(s) in your string is/are \t : ',constant)
+
